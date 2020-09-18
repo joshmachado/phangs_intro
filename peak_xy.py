29e8379c89e063c0f1bbe8736b9d4d6c3920495f
Josh Machado <46457626+joshmachado@users.noreply.github.com>
2020-09-18 19:29:03 -0400

    yup

--- peak_xy.py
+++ peak_xy.py
@@ -1,1 +1,8 @@
 from astropy.table import Table
+import matplotlib.pyplot as plt
+import numpy as np
+
+fp = '/usr/josh/projects/intro/ngc3621_12m+7m+tp_co21_90pc_props.fits.bz2'
+#fp = '/data/kant/0/sun.1608/PHANGS/ALMA/v3p4-CPROPS/STv1p5/90pc_homogenized/ngc3621_12m+7m+tp_co21_90pc_props.fits.bz2'
+
+tab = Table.read(fp)
