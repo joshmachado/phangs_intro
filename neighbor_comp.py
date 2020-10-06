8e4925afa030e50cebbee1aaa9605ac549a72fe6
Josh Machado <46457626+joshmachado@users.noreply.github.com>
2020-10-06 15:54:16 -0400

    beam comp plots

--- neighbor_comp.py
+++ neighbor_comp.py
@@ -9,29 +9,30 @@ cat90 = pd.read_csv('ngc3621_90pc_cloud_stats.csv')
 cat120 = pd.read_csv('ngc3621_120pc_cloud_stats.csv')
 cat150 = pd.read_csv('ngc3621_150pc_cloud_stats.csv')
 
-first60 = cat60['min_dist']
-second60 = cat60['min_dist2nd']
-third60 = cat60['min_dist3rd']
+first60 = cat60['beam_sep_nn']
+second60 = cat60['beam_sep_nn2']
+third60 = cat60['beam_sep_nn3']
 
-first90 = cat90['min_dist']
-second90 = cat90['min_dist2nd']
-third90 = cat90['min_dist3rd']
+first90 = cat90['beam_sep_nn']
+second90 = cat90['beam_sep_nn2']
+third90 = cat90['beam_sep_nn3']
 
-first120 = cat120['min_dist']
-second120 = cat120['min_dist2nd']
-third120 = cat120['min_dist3rd']
+first120 = cat120['beam_sep_nn']
+second120 = cat120['beam_sep_nn2']
+third120 = cat120['beam_sep_nn3']
 
-first150 = cat150['min_dist']
-second150 = cat150['min_dist2nd']
-third150 = cat150['min_dist3rd']
+first150 = cat150['beam_sep_nn']
+second150 = cat150['beam_sep_nn2']
+third150 = cat150['beam_sep_nn3']
 
 
-plt.scatter(first60, third60, label='60 pc', alpha = 0.25)
-plt.scatter(first90, third90, label='90 pc', alpha = 0.25)
-plt.scatter(first120, third120, label='120 pc', alpha = 0.25)
-plt.scatter(first150, third150, label='150 pc', alpha = 0.25)
-plt.xlabel('Distance to First Nearest Neighbor (pc)')
-plt.ylabel('Distance to Third Nearest Neighbor (pc)')
-plt.xlim(0,600)
-plt.ylim(0,800)
-plt.savefig('first_vs_third_pc.pdf')
+plt.scatter(second60, third60, label='60 pc', alpha = 0.25)
+plt.scatter(second90, third90, label='90 pc', alpha = 0.25)
+plt.scatter(second120, third120, label='120 pc', alpha = 0.25)
+plt.scatter(second150, third150, label='150 pc', alpha = 0.25)
+plt.xlabel('Distance to Second Nearest Neighbor (beams)')
+plt.ylabel('Distance to Third Nearest Neighbor (beams)')
+plt.xlim(0,12)
+plt.ylim(0,15)
+plt.show()
+plt.savefig('second_vs_third_beam.pdf')
