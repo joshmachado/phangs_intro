df96e405a8fd6af3d01ac12b4206c8f8b5c8b432
Josh Machado <46457626+joshmachado@users.noreply.github.com>
2020-10-19 10:22:51 -0400

    color adjustments

--- keyplots.py
+++ keyplots.py
@@ -5,7 +5,7 @@ import matplotlib.cm as cm
 import pandas as pd
 import matplotlib.lines as mlines
 
-loc = '/Users/josh/projects/intro'
+loc = '/Users/josh/projects/intro/ngc3621/'
 
 sep = np.zeros([3,3,4,2])
 msep = np.zeros([3,3,4,2])
@@ -14,7 +14,7 @@ merr = np.zeros([3,3,2,4])
 res = [60,90,120,150]
 source = 'ngc3621'
 for i in range(len(res)):
-    cat = pd.read_csv(str(source)+'_'+str(res[i])+'pc_cloud_stats.csv')
+    cat = pd.read_csv(loc+str(source)+'_'+str(res[i])+'pc_cloud_stats.csv')
     match_cat = pd.read_csv(loc+'/matched/'+str(source)+'_'+str(res[i])+'pc_cloud_stats.csv')
 
     #Separation in PC
@@ -79,26 +79,26 @@ for i in range(len(res)):
 
 
 #FORMATTING LEGEND
-first = mlines.Line2D([], [], color='blue', lw=1, label='1st Nearest Neighbor')
-second = mlines.Line2D([], [], color='red', lw=1, label='2nd Nearest Neighbor')
-third = mlines.Line2D([], [], color='green', lw=1, label='3rd Nearest Neighbor')
+first = mlines.Line2D([], [], color='#377eb8', lw=1, label='1st Nearest Neighbor')
+second = mlines.Line2D([], [], color='#4daf4a', lw=1, label='2nd Nearest Neighbor')
+third = mlines.Line2D([], [], color='#a65628', lw=1, label='3rd Nearest Neighbor')
 homogenized = mlines.Line2D([], [], color='black', marker='o', lw=0,  label='Homogenized')
 matched = mlines.Line2D([], [], color='black', marker='s', lw=0,  label='Matched')
 
 #PLOTTING PC SEPERATION DATA
 plt.figure(figsize=(10,10))
-plt.scatter(sep[0,0,:,0]-8, sep[0,0,:,1], c='blue', label='1st Homogenized')
-plt.errorbar(sep[0,0,:,0]-8, sep[0,0,:,1], c='blue', xerr=None, yerr=err[0,0], capsize=4, ls='none')
-plt.scatter(msep[0,0,:,0]-6, msep[0,0,:,1], c='blue', marker='s', label='1st Matched')
-plt.errorbar(msep[0,0,:,0]-6, msep[0,0,:,1], c='blue', xerr=None, yerr=merr[0,0], capsize=4, ls='none')
-plt.scatter(sep[0,1,:,0]-1, sep[0,1,:,1], c='red', label='2nd Homogenized')
-plt.errorbar(sep[0,1,:,0]-1, sep[0,1,:,1], c='red', xerr=None, yerr=err[0,1], capsize=4, ls='none')
-plt.scatter(msep[0,1,:,0]+1, msep[0,1,:,1], c='red', marker='s', label='2nd Matched')
-plt.errorbar(msep[0,1,:,0]+1, msep[0,1,:,1], c='red', xerr=None, yerr=merr[0,1], capsize=4, ls='none')
-plt.scatter(sep[0,2,:,0]+5, sep[0,2,:,1], c='green', label='3rd Homogenized')
-plt.errorbar(sep[0,2,:,0]+5, sep[0,2,:,1], c='green', xerr=None, yerr=err[0,2], capsize=4, ls='none')
-plt.scatter(msep[0,2,:,0]+7, msep[0,2,:,1], c='green', marker='s', label='3rd Matched')
-plt.errorbar(msep[0,2,:,0]+7, msep[0,2,:,1], c='green', xerr=None, yerr=merr[0,2], capsize=4, ls='none')
+plt.scatter(sep[0,0,:,0]-8, sep[0,0,:,1], c='#377eb8', label='1st Homogenized')
+plt.errorbar(sep[0,0,:,0]-8, sep[0,0,:,1], c='#377eb8', xerr=None, yerr=err[0,0], capsize=4, ls='none')
+plt.scatter(msep[0,0,:,0]-6, msep[0,0,:,1], c='#377eb8', marker='s', label='1st Matched')
+plt.errorbar(msep[0,0,:,0]-6, msep[0,0,:,1], c='#377eb8', xerr=None, yerr=merr[0,0], capsize=4, ls='none')
+plt.scatter(sep[0,1,:,0]-1, sep[0,1,:,1], c='#4daf4a', label='2nd Homogenized')
+plt.errorbar(sep[0,1,:,0]-1, sep[0,1,:,1], c='#4daf4a', xerr=None, yerr=err[0,1], capsize=4, ls='none')
+plt.scatter(msep[0,1,:,0]+1, msep[0,1,:,1], c='#4daf4a', marker='s', label='2nd Matched')
+plt.errorbar(msep[0,1,:,0]+1, msep[0,1,:,1], c='#4daf4a', xerr=None, yerr=merr[0,1], capsize=4, ls='none')
+plt.scatter(sep[0,2,:,0]+5, sep[0,2,:,1], c='#a65628', label='3rd Homogenized')
+plt.errorbar(sep[0,2,:,0]+5, sep[0,2,:,1], c='#a65628', xerr=None, yerr=err[0,2], capsize=4, ls='none')
+plt.scatter(msep[0,2,:,0]+7, msep[0,2,:,1], c='#a65628', marker='s', label='3rd Matched')
+plt.errorbar(msep[0,2,:,0]+7, msep[0,2,:,1], c='#a65628', xerr=None, yerr=merr[0,2], capsize=4, ls='none')
 
 #AXIS & LABELS
 plt.legend(handles=[first, second, third, homogenized, matched])
@@ -106,23 +106,23 @@ plt.xlabel('Resolution (pc)')
 plt.ylabel('Distance to Nearest Neighbor (pc)')
 plt.xticks([60,90,120,150])
 plt.title(str(source).upper(), fontsize=16)
-plt.savefig(loc+'/plots/'+str(source)+'_pc_sep_summary.pdf')
+plt.savefig(loc+'plots/'+str(source)+'_pc_sep_summary.pdf')
 plt.close()
 
 #PLOTTING BEAM SEPERATION DATA
 plt.figure(figsize=(10,10))
-plt.scatter(sep[1,0,:,0]-8, sep[1,0,:,1], c='blue', label='1st Homogenized')
-plt.errorbar(sep[1,0,:,0]-8, sep[1,0,:,1], c='blue', xerr=None, yerr=err[1,0], capsize=4, ls='none')
-plt.scatter(msep[1,0,:,0]-6, msep[1,0,:,1], c='blue', marker='s', label='1st Matched')
-plt.errorbar(msep[1,0,:,0]-6, msep[1,0,:,1], c='blue', xerr=None, yerr=merr[1,0], capsize=4, ls='none')
-plt.scatter(sep[1,1,:,0]-1, sep[1,1,:,1], c='red', label='2nd Homogenized')
-plt.errorbar(sep[1,1,:,0]-1, sep[1,1,:,1], c='red', xerr=None, yerr=err[1,1], capsize=4, ls='none')
-plt.scatter(msep[1,1,:,0]+1, msep[1,1,:,1], c='red', marker='s', label='2nd Matched')
-plt.errorbar(msep[1,1,:,0]+1, msep[1,1,:,1], c='red', xerr=None, yerr=merr[1,1], capsize=4, ls='none')
-plt.scatter(sep[1,2,:,0]+5, sep[1,2,:,1], c='green', label='3rd Homogenized')
-plt.errorbar(sep[1,2,:,0]+5, sep[1,2,:,1], c='green', xerr=None, yerr=err[1,2], capsize=4, ls='none')
-plt.scatter(msep[1,2,:,0]+7, msep[1,2,:,1], c='green', marker='s', label='3rd Matched')
-plt.errorbar(msep[1,2,:,0]+7, msep[1,2,:,1], c='green', xerr=None, yerr=merr[1,2], capsize=4, ls='none')
+plt.scatter(sep[1,0,:,0]-8, sep[1,0,:,1], c='#377eb8', label='1st Homogenized')
+plt.errorbar(sep[1,0,:,0]-8, sep[1,0,:,1], c='#377eb8', xerr=None, yerr=err[1,0], capsize=4, ls='none')
+plt.scatter(msep[1,0,:,0]-6, msep[1,0,:,1], c='#377eb8', marker='s', label='1st Matched')
+plt.errorbar(msep[1,0,:,0]-6, msep[1,0,:,1], c='#377eb8', xerr=None, yerr=merr[1,0], capsize=4, ls='none')
+plt.scatter(sep[1,1,:,0]-1, sep[1,1,:,1], c='#4daf4a', label='2nd Homogenized')
+plt.errorbar(sep[1,1,:,0]-1, sep[1,1,:,1], c='#4daf4a', xerr=None, yerr=err[1,1], capsize=4, ls='none')
+plt.scatter(msep[1,1,:,0]+1, msep[1,1,:,1], c='#4daf4a', marker='s', label='2nd Matched')
+plt.errorbar(msep[1,1,:,0]+1, msep[1,1,:,1], c='#4daf4a', xerr=None, yerr=merr[1,1], capsize=4, ls='none')
+plt.scatter(sep[1,2,:,0]+5, sep[1,2,:,1], c='#a65628', label='3rd Homogenized')
+plt.errorbar(sep[1,2,:,0]+5, sep[1,2,:,1], c='#a65628', xerr=None, yerr=err[1,2], capsize=4, ls='none')
+plt.scatter(msep[1,2,:,0]+7, msep[1,2,:,1], c='#a65628', marker='s', label='3rd Matched')
+plt.errorbar(msep[1,2,:,0]+7, msep[1,2,:,1], c='#a65628', xerr=None, yerr=merr[1,2], capsize=4, ls='none')
 
 #AXIS & LABELS
 plt.legend(handles=[first, second, third, homogenized, matched])
@@ -131,23 +131,23 @@ plt.ylabel('Distance to Nearest Neighbor (beams)')
 plt.ylim(0,10)
 plt.xticks([60,90,120,150])
 plt.title(str(source).upper(), fontsize=16)
-plt.savefig(loc+'/plots/'+str(source)+'_beam_sep_summary.pdf')
+plt.savefig(loc+'plots/'+str(source)+'_beam_sep_summary.pdf')
 plt.close()
 
 #PLOTTING MEAN CLOUD RADII SEPERATION DATA
 plt.figure(figsize=(10,10))
-plt.scatter(sep[2,0,:,0]-8, sep[2,0,:,1], c='blue', label='1st Homogenized')
-plt.errorbar(sep[2,0,:,0]-8, sep[2,0,:,1], c='blue', xerr=None, yerr=err[2,0], capsize=4, ls='none')
-plt.scatter(msep[2,0,:,0]-6, msep[2,0,:,1], c='blue', marker='s', label='1st Matched')
-plt.errorbar(msep[2,0,:,0]-6, msep[2,0,:,1], c='blue', xerr=None, yerr=merr[2,0], capsize=4, ls='none')
-plt.scatter(sep[2,1,:,0]-1, sep[2,1,:,1], c='red', label='2nd Homogenized')
-plt.errorbar(sep[2,1,:,0]-1, sep[2,1,:,1], c='red', xerr=None, yerr=err[2,1], capsize=4, ls='none')
-plt.scatter(msep[2,1,:,0]+1, msep[2,1,:,1], c='red', marker='s', label='2nd Matched')
-plt.errorbar(msep[2,1,:,0]+1, msep[2,1,:,1], c='red', xerr=None, yerr=merr[2,1], capsize=4, ls='none')
-plt.scatter(sep[2,2,:,0]+5, sep[2,2,:,1], c='green', label='3rd Homogenized')
-plt.errorbar(sep[2,2,:,0]+5, sep[2,2,:,1], c='green', xerr=None, yerr=err[2,2], capsize=4, ls='none')
-plt.scatter(msep[2,2,:,0]+7, msep[2,2,:,1], c='green', marker='s', label='3rd Matched')
-plt.errorbar(msep[2,2,:,0]+7, msep[2,2,:,1], c='green', xerr=None, yerr=merr[2,2], capsize=4, ls='none')
+plt.scatter(sep[2,0,:,0]-8, sep[2,0,:,1], c='#377eb8', label='1st Homogenized')
+plt.errorbar(sep[2,0,:,0]-8, sep[2,0,:,1], c='#377eb8', xerr=None, yerr=err[2,0], capsize=4, ls='none')
+plt.scatter(msep[2,0,:,0]-6, msep[2,0,:,1], c='#377eb8', marker='s', label='1st Matched')
+plt.errorbar(msep[2,0,:,0]-6, msep[2,0,:,1], c='#377eb8', xerr=None, yerr=merr[2,0], capsize=4, ls='none')
+plt.scatter(sep[2,1,:,0]-1, sep[2,1,:,1], c='#4daf4a', label='2nd Homogenized')
+plt.errorbar(sep[2,1,:,0]-1, sep[2,1,:,1], c='#4daf4a', xerr=None, yerr=err[2,1], capsize=4, ls='none')
+plt.scatter(msep[2,1,:,0]+1, msep[2,1,:,1], c='#4daf4a', marker='s', label='2nd Matched')
+plt.errorbar(msep[2,1,:,0]+1, msep[2,1,:,1], c='#4daf4a', xerr=None, yerr=merr[2,1], capsize=4, ls='none')
+plt.scatter(sep[2,2,:,0]+5, sep[2,2,:,1], c='#a65628', label='3rd Homogenized')
+plt.errorbar(sep[2,2,:,0]+5, sep[2,2,:,1], c='#a65628', xerr=None, yerr=err[2,2], capsize=4, ls='none')
+plt.scatter(msep[2,2,:,0]+7, msep[2,2,:,1], c='#a65628', marker='s', label='3rd Matched')
+plt.errorbar(msep[2,2,:,0]+7, msep[2,2,:,1], c='#a65628', xerr=None, yerr=merr[2,2], capsize=4, ls='none')
 
 #AXIS & LABELS
 plt.legend(handles=[first, second, third, homogenized, matched])
@@ -155,5 +155,5 @@ plt.xlabel('Resolution (pc)')
 plt.ylabel('Distance to Nearest Neighbor (mean cloud radii)')
 plt.xticks([60,90,120,150])
 plt.title(str(source).upper(), fontsize=16)
-plt.savefig(loc+'/plots/'+str(source)+'_cloud_sep_summary.pdf')
+plt.savefig(loc+'plots/'+str(source)+'_cloud_sep_summary.pdf')
 plt.close()
