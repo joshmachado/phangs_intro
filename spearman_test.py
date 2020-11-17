8af10362a7f3dc38e66ceeaa42397c1d34562d94
Josh Machado <46457626+joshmachado@users.noreply.github.com>
2020-11-17 14:56:27 -0500

    additional corr_plots

--- spearman_test.py
+++ spearman_test.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 sources = ['ngc1433', 'ngc3621', 'ngc6300', 'ngc2835']
 res = [60, 90, 120, 150]
-prop = ['MLUM_MSUN', 'SIGV_KMS', 'RAD_PC']
+prop = ['MLUM_MSUN', 'SIGV_KMS', 'RAD_NODC']
 stats = np.zeros([len(prop), 6])
 for i in range(len(res)):
     df = pd.DataFrame(columns = ('source', 'first_second', 'pval_first_second', 'first_third', 'pval_first_third',
@@ -19,15 +19,21 @@ for i in range(len(res)):
                 first = np.array(cat[str(prop[k])+'_nn'])
                 second = np.array(cat[str(prop[k])+'_nn2'])
                 third = np.array(cat[str(prop[k])+'_nn3'])
+                fs = scipy.stats.spearmanr(first, second, nan_policy='omit')
+                ft = scipy.stats.spearmanr(first, third, nan_policy='omit')
+                st = scipy.stats.spearmanr(second, third, nan_policy='omit')
                 plt.scatter(first, second)
                 plt.xlabel(prop[k])
                 plt.ylabel(prop[k])
                 plt.title(sources[j]+' '+str(res[i])+'pc resolution - ' + prop[k])
-                plt.savefig('/Users/josh/projects/intro/stats/corr_plots/'+prop[k]+'_'+str(res[i])+'pc_'+sources[j]+'.pdf')
+                plt.savefig('/Users/josh/projects/intro/stats/corr_plots/first_second/'+prop[k]+'_'+str(res[i])+'pc_'+sources[j]+'.pdf')
                 plt.close()
-                fs = scipy.stats.spearmanr(first, second, nan_policy='omit')
-                ft = scipy.stats.spearmanr(first, third, nan_policy='omit')
-                st = scipy.stats.spearmanr(second, third, nan_policy='omit')
+                plt.scatter(second, third)
+                plt.xlabel(prop[k])
+                plt.ylabel(prop[k])
+                plt.title(sources[j]+' '+str(res[i])+'pc resolution - ' + prop[k])
+                plt.savefig('/Users/josh/projects/intro/stats/corr_plots/first_second/'+prop[k]+'_'+str(res[i])+'pc_'+sources[j]+'.pdf')
+                plt.close()
                 stats[k] = [fs[0], fs[1], ft[0], ft[1], st[0], st[1]]
                 df.loc[j] = [str(sources[j]), stats[0][0], stats[0][1], stats[0][2], stats[0][3], stats[0][4], stats[0][5]]
                 df.to_csv('/Users/josh/projects/intro/stats/'+str(prop[k])+'_spearman_coef_'+str(res[i])+'pc.csv', index=False)
