d0d441a74422a8e7cbc992a7e54aaf687d14fbf5
joshmachado <josh.a.machado@gmail.com>
2020-11-17 15:40:19 -0500

    updated prop corr summary plots

--- comp_corr_plots.py
+++ comp_corr_plots.py
@@ -3,6 +3,7 @@ import scipy.stats
 import pandas as pd
 import os.path
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 
 sources = ['ngc1433', 'ngc3621', 'ngc6300', 'ngc2835']
 res = [60, 90, 120, 150]
@@ -20,9 +21,12 @@ for i in range(len(res)):
                 first = np.array(cat[str(prop[k])+'_nn'])
                 second = np.array(cat[str(prop[k])+'_nn2'])
                 third = np.array(cat[str(prop[k])+'_nn3'])
-                axes[k,0].scatter(first, second)
-                axes[k,1].scatter(second, third)
+                #box = Rectangle((0, 0), 1, 1, fc="w", fill=False, edgecolor='none', linewidth=0)
+                axes[k,0].scatter(first, second, label=str(scipy.stats.spearmanr(first, second, nan_policy='omit')[0]))
+                axes[k,1].scatter(second, third, label=str(scipy.stats.spearmanr(second, third, nan_policy='omit')[0]))
                 axes[k,0].set_ylabel(prop[k])
+                axes[k,0].legend()
+                axes[k,1].legend()
             axes[0,0].set_title('First to Second')
             axes[0,1].set_title('Second to Third')
             fig.suptitle(sources[j]+' at '+str(res[i])+'pc resolution', fontsize=16)
